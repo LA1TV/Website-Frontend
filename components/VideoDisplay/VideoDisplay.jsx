8d6bea900443634ae5da92dee8fc3d653a88b546
M3lNic <melody.astrid.nicholls@gmail.com>
2019-07-23 00:51:07 +0100

    Replaced ReactPlayer with iframe embed

--- components/VideoDisplay/VideoDisplay.jsx
+++ components/VideoDisplay/VideoDisplay.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
-import ReactPlayer from 'react-player';
-
-//const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 const VideoDisplay = ({ children, className, href }) => (
-  <ReactPlayer url={["https://www.la1tv.co.uk/file/61690"]} controls="true" allowFullScreen light="https://www.la1tv.co.uk/file/26264"/>
+  // <ReactPlayer url={["https://www.la1tv.co.uk/file/61690"]} controls="true" allowFullScreen light="https://www.la1tv.co.uk/file/26264"/>
+  <iframe src="https://embed.la1tv.co.uk/328"></iframe>
 );
   
 export default VideoDisplay;
