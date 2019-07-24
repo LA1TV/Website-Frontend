1bc47ac8249f17a81cf7aa3e76d9ff579c78b051
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-07-24 01:31:05 +0100

    make simple render test

--- test/index.test.jsx
+++ test/index.test.jsx
@@ -1,15 +1,6 @@
-import React from 'react'
-import { mount } from 'enzyme'
-import toJson from 'enzyme-to-json'
+import { renderToJson } from 'tooling/jest/render'
 import Index from 'pages/index.js'
 
-const render = (props = {}) => {
-  return mount(<Index {...props} />)
-}
-
-describe('index', () => {
-  it('should be able to render', () => {
-    const wrapper = render()
-    expect(toJson(wrapper)).toMatchSnapshot()
-  })
+test('should be able to render', () => {
+  expect(renderToJson(Index)).toMatchSnapshot()
 })
