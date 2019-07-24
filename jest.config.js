3a3cde4c4a522610ad2afdc8819b507a027fe8d1
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-07-24 01:20:36 +0100

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const ignorePaths = [
+  'babel.config.js',
+  'jest.config.js',
+  'node_modules',
+  '.next'
+]
+
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/**/*{.js,x}'],
+  coveragePathIgnorePatterns: [...ignorePaths],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
+  clearMocks: true,
+  setupFiles: ['<rootDir>/tooling/jest/setup.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testMatch: ['**/?(*.)test.js?(x)'],
+  testPathIgnorePatterns: [...ignorePaths],
+  watchPathIgnorePatterns: [...ignorePaths]
+}
