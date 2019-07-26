4be630f8013698150d0908aaf05b7911f06d633c
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-07-26 23:43:50 +0100

    cahnge to emoji tick

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.prepare().then(() => {
     app.render(req, res, pathname, query)
   }).listen(port, err => {
     if (err) throw err
-    console.log(`> Ready on http://localhost:${port}`)
+    console.log(`✅ Ready on http://localhost:${port}`)
   })
 })
