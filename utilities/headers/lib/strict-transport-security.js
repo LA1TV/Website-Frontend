d74c39ab97c756da7e310d93f1ed853e0833a194
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-07-27 00:35:12 +0100

    set hsts policy to 6 months as per mozilla guidelines

--- utilities/headers/lib/strict-transport-security.js
+++ utilities/headers/lib/strict-transport-security.js
@@ -1,4 +1,4 @@
-const cacheInSecond = 5256000
+const cacheInSecond = 15768000
 
 const hstsHeader = ['Strict-Transport-Security', `max-age=${cacheInSecond}`]
 
--- utilities/headers/test/sets-headers.test.js
+++ utilities/headers/test/sets-headers.test.js
@@ -9,7 +9,7 @@ test('sets headers', () => {
 
   const expectedFeaturePolicy = "ambient-light-sensor 'none'; autoplay 'none'; accelerometer 'none'; camera 'none'; encrypted-media 'none'; fullscreen 'none'; geolocation 'none'; gyroscope 'none'; magnetometer 'none'; microphone 'none'; midi 'none'; payment 'none'; picture-in-picture 'none'; speaker 'none'; sync-xhr 'none'; usb 'none'; vr 'none';"
   const expectedContentSecurityPolicy = "default-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; img-src 'self'; frame-src embed.la1tv.co.uk; object-src 'none'; worker-src 'self'; manifest-src 'self'; base-uri 'none'; form-action 'none'; frame-ancestors 'none'; connect-src 'self';"
-  expect(mockRes.setHeader).toHaveBeenCalledWith('Strict-Transport-Security', `max-age=5256000`)
+  expect(mockRes.setHeader).toHaveBeenCalledWith('Strict-Transport-Security', `max-age=15768000`)
   expect(mockRes.setHeader).toHaveBeenCalledWith('X-Content-Type-Options', 'nosniff')
   expect(mockRes.setHeader).toHaveBeenCalledWith('Referrer-Policy', 'no-referrer')
   expect(mockRes.setHeader).toHaveBeenCalledWith('Feature-Policy', expectedFeaturePolicy)
