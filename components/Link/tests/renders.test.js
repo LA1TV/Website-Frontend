f4093c8da773472239c4dd04e67aa47367a20702
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-07-27 17:40:59 +0100

    fix linting

--- components/Link/tests/renders.test.js
+++ components/Link/tests/renders.test.js
@@ -1,20 +1,20 @@
-import { renderToJson } from "tooling/jest/render";
-import Link, { NavigationLink } from "..";
+import { renderToJson } from 'tooling/jest/render'
+import Link, { NavigationLink } from '..'
 
-test("renders", () => {
+test('renders', () => {
   expect(renderToJson(Link)).toMatchInlineSnapshot(`
     <Link
       prefetch={true}
     >
       <Link__StyledLink />
     </Link>
-  `);
-});
+  `)
+})
 
-test("renders navigation", () => {
+test('renders navigation', () => {
   expect(renderToJson(NavigationLink)).toMatchInlineSnapshot(`
     <NavigationLink__StyledListItem>
       <NavigationLink__StyledLink />
     </NavigationLink__StyledListItem>
-  `);
-});
+  `)
+})
