bcb5cb047189a77615ddf0432cd0d4eefd0768b0
M3lNic <melody.astrid.nicholls@gmail.com>
2019-09-01 15:55:33 +0100

    api changes

--- pages/api/watch.js
+++ pages/api/watch.js
@@ -1,5 +1,4 @@
-import fetch from 'isomorphic-unfetch'
-require('dotenv').config()
+import api_call from './api_call'
 
 const parseResponse = ({ mediaItem: { name, embed: { iframeUrl } } }) => ({
   name,
@@ -7,26 +6,15 @@ const parseResponse = ({ mediaItem: { name, embed: { iframeUrl } } }) => ({
 })
 
 const handle = async ({ query: { videoId } }, res) => {
-  const url = `https://www.la1tv.co.uk/api/v1/mediaItems/${videoId}`
+  const url = `mediaItems/${videoId}`
 
-  const la1tv = await fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Api-Key': process.env.LA1TV_API_KEY
-    }
-  })
-
-  const { data } = await la1tv.json()
-
-  if (data === undefined) {
-    res.status(404).send('Video not found :(')
-    return
-  }
-
-  const parsedResponse = parseResponse(data)
-
-  res.send(parsedResponse)
+  await api_call(url).then((data) => {
+    const parsedResponse = parseResponse(data)
+    res.send(parsedResponse)
+  }).catch((err) => {
+    console.log(err)
+    res.status(404).send(err)
+  })  
 }
 
 export default handle
