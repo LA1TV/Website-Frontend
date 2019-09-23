e4e94b96c9e2c2348241124304cb786bcc06edbb
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-09-23 22:54:11 +0100

    make ci pass for now

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,7 +1,7 @@
 import { la1tvFetcher } from 'utilities/api'
 
 const handle = async ({ query: { amount = '24', offset = '0', page = '1' } }, res) => {
-  const url = `mediaItems?limit=6`
+  const url = 'mediaItems?limit=6'
 
   const { statusCode, body } = await la1tvFetcher({ url })
 
