c90519746b05d385d481721ec91107e5339a0be8
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-09-24 18:11:46 +0100

    fix linting

--- test/pages/watch.test.jsx
+++ test/pages/watch.test.jsx
@@ -1,47 +1,47 @@
-import { renderToJson } from "tooling/jest/render";
-import Watch from "pages/watch/[videoId].js";
-import fetch from "isomorphic-unfetch";
+import { renderToJson } from 'tooling/jest/render'
+import Watch from 'pages/watch/[videoId].js'
+import fetch from 'isomorphic-unfetch'
 
-jest.mock("isomorphic-unfetch");
+jest.mock('isomorphic-unfetch')
 
-test("calls for data from the api", async () => {
+test('calls for data from the api', async () => {
   fetch.mockReturnValue({
     status: 200,
     json: () => ({
-      some: "object"
+      some: 'object'
     })
-  });
+  })
 
-  expect(await Watch.getInitialProps({ query: { videoId: "someId" } }))
+  expect(await Watch.getInitialProps({ query: { videoId: 'someId' } }))
     .toEqual({
-      "some": "object",
-      "statusCode": 200,
-    });
-});
+      some: 'object',
+      statusCode: 200
+    })
+})
 
 test('just returns statusCode on none 200 response', async () => {
-  fetch.mockReturnValue({ status: 404 });
-  
-  expect(await Watch.getInitialProps({ query: { videoId: "someId" } }))
-    .toEqual({
-      "statusCode": 404,
-    });
-});
+  fetch.mockReturnValue({ status: 404 })
 
-test("should handle error pages", () => {
+  expect(await Watch.getInitialProps({ query: { videoId: 'someId' } }))
+    .toEqual({
+      statusCode: 404
+    })
+})
+
+test('should handle error pages', () => {
   expect(renderToJson(Watch, { statusCode: 404 })).toMatchInlineSnapshot(`
     <ErrorPage
       statusCode={404}
     />
-  `);
-});
+  `)
+})
 
-test("should handle renders", () => {
+test('should handle renders', () => {
   expect(
     renderToJson(Watch, {
       statusCode: 200,
-      name: "someName",
-      description: "someDescription"
+      name: 'someName',
+      description: 'someDescription'
     })
   ).toMatchInlineSnapshot(`
     <Center>
@@ -61,5 +61,5 @@ test("should handle renders", () => {
         }
       />
     </Center>
-  `);
-});
+  `)
+})
