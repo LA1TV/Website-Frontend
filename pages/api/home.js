fc838594c52c6fc8d192e09f8ccfa87df1768dd5
benjaminkay93 <ben.kay01@hotmail.co.uk>
2019-10-07 21:03:42 +0100

    remove page from home

--- pages/api/home.js
+++ pages/api/home.js
@@ -1,6 +1,6 @@
 import { la1tvFetcher } from 'utilities/api'
 
-const handle = async ({ query: { amount = '24', offset = '0', page = '1' } }, res) => {
+const handle = async ({ query: { amount = '24', offset = '0' } }, res) => {
   const url = 'mediaItems?limit=6'
 
   const { statusCode, body } = await la1tvFetcher({ url })
