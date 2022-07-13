d79a39b4f6c4a554ebfae097768fa50b02fbfe6c
Jack Dunn <56315967+JackDunnCodes@users.noreply.github.com>
2022-07-13 17:09:55 +0100

    Homepage test

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import Paragraph from 'components/Paragraph'
+import VideoLink from 'components/VideoLink'
+import Row from '../layouts/Row'
 
-const Index = () => (
+const Index = () => (<>
   <Paragraph>Hello Next.js</Paragraph>
+  <Row recursive children={[
+    <VideoLink title="test" description="test" poster="http://localhost:8000/v1/s3/thumbnails/good%20owl.jpg"/>,
+    <VideoLink title="test" description="test" poster="http://localhost:8000/v1/s3/thumbnails/good%20owl.jpg"/>,
+  ]}>
+  </Row>
+  </>
 )
 
 export default Index
